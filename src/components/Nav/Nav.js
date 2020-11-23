eca0ef31979d60b678ffa8881a8d94512f3ea10a
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-11-24 00:49:54 +0100

    Remove redundant header

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,20 +5,18 @@ import './Nav.css';
 export default function Nav() {
 	const [open, setOpen] = useState(true);
 	return (
-		<header>
-			<nav className='nav'>
-				<a title='main page' href='/' className='logo'>
-					<span>Abdellatif Elaroussi</span>
-				</a>
-				<span className={`nav-toggle ${open ? 'X' : 'sticks'}`}>X</span>
-				<ul className='nav-links'>
-					<li>
-						<a title='Services' href='#services'>
-							Services
-						</a>
-					</li>
-				</ul>
-			</nav>
-		</header>
+		<nav className='nav'>
+			<a title='main page' href='/' className='logo'>
+				<span>Abdellatif Elaroussi</span>
+			</a>
+			<span className={`nav-toggle ${open ? 'X' : 'sticks'}`}>X</span>
+			<ul className='nav-links'>
+				<li>
+					<a title='Services' href='#services'>
+						Services
+					</a>
+				</li>
+			</ul>
+		</nav>
 	);
 }
