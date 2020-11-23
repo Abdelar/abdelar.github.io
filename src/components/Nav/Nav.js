7ff8137c474168190541158034489039170e6c06
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-11-24 00:35:10 +0100

    Add a toggle variable for the navigation menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+
+import './Nav.css';
+
 export default function Nav() {
-	return <div>nav</div>;
+	const [open, setOpen] = useState(true);
+	return (
+		<header>
+			<nav className='nav'>
+				<a title='main page' href='/' className='logo'>
+					<span>Abdellatif Elaroussi</span>
+				</a>
+				<span className={`nav-toggle ${open ? 'X' : 'sticks'}`}>X</span>
+				<ul className='nav-links'>
+					<li>
+						<a title='Services' href='#services'>
+							Services
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
