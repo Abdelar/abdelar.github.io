772b2dea8e35165320e82038bd05d8efb1ac1b6f
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-11-24 20:03:20 +0100

    Fix nav to the top

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,6 +7,12 @@ export default function Main() {
 			<header>
 				<Nav />
 			</header>
+			<p className='test'>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt deleniti
+				consequuntur iusto, doloribus non animi eum odit numquam nulla?
+				Similique enim reiciendis voluptate quia officiis hic pariatur
+				accusantium recusandae exercitationem!
+			</p>
 		</>
 	);
 }
