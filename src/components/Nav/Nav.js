4140b76d156e9c7a897952e7d1f9356025998f02
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-11-24 04:18:45 +0100

    Set up navigation for mobile devices

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,14 +3,37 @@ import { useState } from 'react';
 import './Nav.css';
 
 export default function Nav() {
-	const [open, setOpen] = useState(true);
+	const [open, setOpen] = useState(false);
+
+	const toggleNav = () => {
+		setOpen(!open);
+	};
 	return (
-		<nav className='nav'>
+		<nav className={`nav ${open ? 'open' : 'closed'}`}>
 			<a title='main page' href='/' className='logo'>
 				<span>Abdellatif Elaroussi</span>
 			</a>
-			<span className={`nav-toggle ${open ? 'X' : 'sticks'}`}>X</span>
+			<div
+				className={`nav-toggle ${open ? 'X' : 'sticks'}`}
+				onClick={toggleNav}>
+				<span></span>
+			</div>
 			<ul className='nav-links'>
+				<li>
+					<a title='Services' href='#services'>
+						Services
+					</a>
+				</li>
+				<li>
+					<a title='Services' href='#services'>
+						Services
+					</a>
+				</li>
+				<li>
+					<a title='Services' href='#services'>
+						Services
+					</a>
+				</li>
 				<li>
 					<a title='Services' href='#services'>
 						Services
