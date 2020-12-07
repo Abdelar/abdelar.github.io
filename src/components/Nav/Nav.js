e26fecf818ae7d9fc12335dd8bea391506935e45
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-12-07 19:55:11 +0100

    Hide navbar on scroll

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,24 +1,40 @@
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { logo, navLinks } from '../../config';
 
 import './Nav.css';
 
 export default function Nav() {
+	let positionY = 0;
 	const [open, setOpen] = useState(false);
+	const [isHidden, setIsHidden] = useState(false);
 
-	const toggleNav = () => {
+	const toggleMobileNav = () => {
 		setOpen(!open);
 	};
 
+	const toggleNav = useCallback(() => {
+		setOpen(false);
+		window.scrollY > positionY ? setIsHidden(true) : setIsHidden(false);
+		positionY = window.scrollY;
+	}, [positionY]);
+
+	useEffect(() => {
+		window.addEventListener('scroll', toggleNav);
+		return () => window.removeEventListener('scroll', toggleNav);
+	}, [toggleNav]);
+
 	return (
-		<nav className={`nav ${open ? 'open' : 'closed'}`}>
+		<nav
+			className={`nav ${open ? 'open' : 'closed'} ${
+				isHidden ? 'hideBar' : ''
+			}`}>
 			<a title={logo.title} href={logo.href} className='logo'>
 				<span>{logo.text}</span>
 			</a>
 			<div
 				className={`nav-toggle ${open ? 'X' : 'sticks'}`}
-				onClick={toggleNav}>
+				onClick={toggleMobileNav}>
 				<span></span>
 			</div>
 			<ul className='nav-links'>
