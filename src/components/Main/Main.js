f2f1d9aa3e1871a7701ad3de15ef4798c7bc86a2
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-12-07 19:14:46 +0100

    Fade sections in when on screen

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -25,6 +25,7 @@ import Work from '../Work/Work';
 import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 import './Main.css';
+import { useOnScreen } from '../../hooks/useOnScreen';
 
 library.add(
 	faFile,
@@ -43,26 +44,37 @@ library.add(
 );
 
 export default function Main() {
+	const [setRef2, visible2] = useOnScreen({ threshold: 0.5 });
+	const [setRef3, visible3] = useOnScreen({ threshold: 0.5 });
+	const [setRef4, visible4] = useOnScreen({ threshold: 0.5 });
+	const [setRef5, visible5] = useOnScreen({ threshold: 0.5 });
+	const [setRef6, visible6] = useOnScreen({ threshold: 0.5 });
 	return (
 		<>
 			<header>
 				<Nav />
 			</header>
 			<main>
-				<section id='home'>
+				<section id='home' ref={setRef2} className={visible2 ? 'onScreen' : ''}>
 					<Intro />
 				</section>
-				<section id='about'>
+				<section
+					id='about'
+					ref={setRef3}
+					className={visible3 ? 'onScreen' : ''}>
 					<About />
 				</section>
-				<section id='work'>
+				<section id='work' ref={setRef4} className={visible4 ? 'onScreen' : ''}>
 					<Work />
 				</section>
-				<section id='contact'>
+				<section
+					id='contact'
+					ref={setRef5}
+					className={visible5 ? 'onScreen' : ''}>
 					<Contact />
 				</section>
 			</main>
-			<footer>
+			<footer ref={setRef6} className={visible6 ? 'onScreen' : ''}>
 				<Footer />
 			</footer>
 		</>
---
+++ src/hooks/useOnScreen.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+// from https://usehooks.com/useOnScreen
+export function useOnScreen(options) {
+	const [ref, setRef] = useState(null);
+	const [visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(([entry]) => {
+			setVisible(entry.isIntersecting);
+		}, options);
+		if (ref) {
+			observer.observe(ref);
+		}
+		return () => {
+			if (ref) {
+				observer.unobserve(ref);
+			}
+		};
+	}, [ref, options]);
+
+	return [setRef, visible];
+}
